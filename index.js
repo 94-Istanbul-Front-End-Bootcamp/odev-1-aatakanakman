c056a2c29c5ba830dbaa9a07a616a2265b6a40f4
aatakanakman <48566734+aatakanakman@users.noreply.github.com>
2021-06-24 01:16:06 +0300

    Edited

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
-const getButton = document.getElementById('getButton');
+const getButton = document.getElementById('getButton')
 const nameSurname = document.getElementById('nameSurname');
 const age = document.getElementById('age')
 const linkedin = document.getElementById('linkedinUrl');
 const github = document.getElementById('githubUrl');
 const sifirlaBtn = document.getElementById('sifirla')
-
-
 getButton.addEventListener('click', onclick)
 
 
+//variables
+const myName = "Ali Atakan"
+const mySurname = "AKMAN"
+const myAge = 22
+const socialMedia = {
+   linkedin : "https://www.linkedin.com/in/aatakanakman/",
+   github : "https://github.com/aatakanakman"
+}
+
+
 function onclick() {
-    
-    linkedin.setAttribute("href", "https://www.linkedin.com/in/aatakanakman/")
-    github.setAttribute("href", "https://github.com/aatakanakman")
+
+    //linkedin href attr
+    var linkedinHref = document.createAttribute("href")
+    linkedinHref.value = socialMedia.linkedin
+    linkedin.setAttributeNode(linkedinHref)
+    //github href attr    
+    var githubHref = document.createAttribute("href")
+    githubHref.value = socialMedia.github
+    github.setAttributeNode(githubHref)
+
+    //name, surname and age
+    nameSurname.textContent = `${myName} ${mySurname}`;
+    age.textContent = myAge
+
     getButton.style.display = "none"
-    nameSurname.textContent = "Ali Atakan AKMAN";
-    age.textContent = 22
-    
 }
 
 
